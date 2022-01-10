36a11c6d91da87338cd735a86ef6daca4d224135
TheJoeCoder <joe@radialbog9.uk>
2022-01-10 23:21:55 +0000

    Add some line breaks and a name setter

--- oop-course/text-adventure-game/new-text-adventure.py
+++ oop-course/text-adventure-game/new-text-adventure.py
@@ -18,30 +18,41 @@ class Direction(Enum):
         if self is Direction.WEST:
             return Direction.EAST
 
+
 class Room():
     def __init__(self, name: str) -> None:
         self.name = name
         self.description = "No description"
         self.linkedrooms = {}
+    
     def get_name(self) -> str:
         return self.name
+    
+    def set_name(self, name: str) -> None:
+        self.name = name
+    
     def get_description(self) -> str:
         return self.description
+    
     def set_description(self, description: str) -> None:
         self.description = description
+    
     def get_linked_room(self, direction: Direction):
         try:
             return self.linkedrooms[direction]
         except KeyError:
             return None
+    
     def link_room(self, direction: Direction, room_to_link) -> None:
         self.linkedrooms[direction] = room_to_link
+    
     def print_details(self):
         print(self.name + "\n---\n" + self.description + "\n---")
         for direction in self.linkedrooms:
             room = self.linkedrooms[direction]
             print("The " + room.get_name() + " is " + direction.name.lower())
         print("----------")
+
 
 def link_bidirectional(first: Room, second: Room, direction: Direction):
     first.link_room(direction, second)
