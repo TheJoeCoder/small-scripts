8b41bbebfeb58fd100aa9a468513aedd0fc5660b
TheJoeCoder <joe@radialbog9.uk>
2022-01-13 12:14:10 +0000

    Update shape.py

--- python-code/shape.py
+++ python-code/shape.py
@@ -1,9 +1,8 @@
 from turtle import *
 from math import *
 
-color("seagreen")
-
-sides = 69
+sides = int(input("How many sides?\n> "))
+color(input("What colour?\n> "))
 
 for i in range(sides):
   forward(floor(360/sides/1.02))
